00d3259b28aed0d5198080fc46ea7e5a1b2ecdc9
Darrenni97 <darrenni_@hotmail.com>
2020-02-22 15:14:50 -0800

    item styling

--- src/Item.js
+++ src/Item.js
@@ -2,20 +2,21 @@ import React from "react";
 import "./Item.css";
 
 const Item = ({ item, onAddToCart }) => {
-  <div className="Item">
-    <div className="Item-left">
-      <div className="Item-image">
+  return (
+    <div className="Item">
+      <div className="Item-left">
+        <div className="Item-image" />
         <div className="Item-title">{item.name}</div>
         <div className="Item-description">{item.description}</div>
       </div>
+      <div className="Item-right">
+        <div className="Item-price">${item.price}</div>
+        <button className="Item-addToCart" onClick={onAddToCart}>
+          Add to Cart
+        </button>
+      </div>
     </div>
-    <div className="Item-right">
-      <div className="Item-price">${item.price}</div>
-      <button className="Item-addToCart" onClick={onAddToCart}>
-        Add to Cart
-      </button>
-    </div>
-  </div>;
+  );
 };
 
 export default Item;
